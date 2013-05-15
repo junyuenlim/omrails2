bed0ad0923fd7776414478c1948f9b022cf33b4c
Jun Lim <junyuen@gmail.com>
2013-05-15 19:13:09 +0800

    Add Location to Pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215021502) do
+ActiveRecord::Schema.define(:version => 20130515075601) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130215021502) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "location"
   end
 
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
